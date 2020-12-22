3f328d251704b11496a1bc7c7975e48c0f932e3d
Osaze Shears <oshears@vt.edu>
2020-12-21 19:31:50 -0500

    Update gitCheck.sh

--- _data/gitCheck.sh
+++ _data/gitCheck.sh
@@ -1,7 +1,7 @@
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "github-actions[bot]"
-git add _data/timeline_data.yml
-git add _data/test_timeline.xlsx
+git add timeline_data.yml
+git add test_timeline.xlsx
 if git commit -m "updated timeline_data.yml and test_timeline.xlsx" ; then
     git push
     echo "git add succeeded!"
