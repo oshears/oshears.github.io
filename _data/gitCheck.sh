037fdcf14e9f9e4c3691953c166eede852dcaa9e
Osaze Shears <oshears@vt.edu>
2020-12-21 19:26:37 -0500

    added script to conditionally push if chanes were found

---
+++ _data/gitCheck.sh
@@ -0,0 +1,10 @@
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
+git add _data/timeline_data.yml
+git add _data/test_timeline.xlsx
+if git commit -m "updated timeline_data.yml and test_timeline.xlsx" ; then
+    git push
+    echo "git add succeeded!"
+else
+    echo "git add failed!"
+fi
